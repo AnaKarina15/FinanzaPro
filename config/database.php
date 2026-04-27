c60461011e45f4cf490cd281f37e7288833c8580
Ana Karina <160169962+AnaKarina15@users.noreply.github.com>
2026-04-27 17:57:25 -0500

    feat: add database configuration file with connection constants

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_NAME', 'finanzapro');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+define('DB_SSL_CA', '');
+?>
