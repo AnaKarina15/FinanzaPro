e470cacbfac98afa8d70ae7e41a7a101b4b7dcbd
Ana Karina <160169962+AnaKarina15@users.noreply.github.com>
2026-04-28 21:14:57 -0500

    fix: update database password for security compliance

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ define('DB_HOST', 'finanzapro-db-finanzaprodb.a.aivencloud.com');
 define('DB_PORT', '27260');
 define('DB_NAME', 'defaultdb');
 define('DB_USER', 'avnadmin');
-define('DB_PASS', 'AVNS_7RQJ_AZv-vXEMnjNHVl');
+define('DB_PASS', 'AQUI_VA_LA_PASS');
 
 // Descarga el Certificado CA de Aiven y guárdalo en la carpeta config como ca.pem
 define('DB_SSL_CA', __DIR__ . '/ca.pem');
