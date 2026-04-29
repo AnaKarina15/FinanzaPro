c96bb9e5b0feb329d146760a9a91da133ecc9c3f
afriverag <andresrg0211@gmail.com>
2026-04-28 21:38:23 -0500

    feat: add database configuration file and update gitignore to secure credentials

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => 'finanzapro-db-finanzaprodb.a.aivencloud.com',
-    'puerto' => '27260',
-    'usuario' => 'avnadmin',
-    'contrasena' => 'AQUI_VA_LA_PASS',
-    'base_de_datos' => 'defaultdb'
-];
-
